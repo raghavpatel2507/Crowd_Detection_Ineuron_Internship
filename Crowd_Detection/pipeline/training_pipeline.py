7b4a3ec0d54a5e71f05d98c4d5bc826d0be07cfe
raghav patel <patelraghav014@gmail.com>
2024-04-27 15:53:12 +0530

    model_training addede

--- Crowd_Detection/pipeline/training_pipeline.py
+++ Crowd_Detection/pipeline/training_pipeline.py
@@ -3,21 +3,23 @@ from Crowd_Detection.logger import logging
 from Crowd_Detection.exception import AppException
 from Crowd_Detection.components.data_ingestion import DataIngestion
 from Crowd_Detection.components.data_validation import DataValidation
-
+from Crowd_Detection.components.model_trainer import ModelTrainer
 
 
 from Crowd_Detection.entity.config_entity import (DataIngestionConfig,
-                                                 DataValidationConfig)
+                                                 DataValidationConfig,
+                                                 ModelTrainerConfig)
 
 from Crowd_Detection.entity.artifacts_entity import (DataIngestionArtifact,
-                                                    DataValidationArtifact)
+                                                    DataValidationArtifact,
+                                                    ModelTrainerArtifact)
 
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
-        
+        self.model_trainer_config = ModelTrainerConfig()
 
 
     
@@ -73,7 +75,17 @@ class TrainPipeline:
 
 
     
-   
+    def start_model_trainer(self
+    ) -> ModelTrainerArtifact:
+        try:
+            model_trainer = ModelTrainer(
+                model_trainer_config=self.model_trainer_config,
+            )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise AppException(e, sys)
         
         
 
@@ -87,6 +99,12 @@ class TrainPipeline:
             data_validation_artifact = self.start_data_validation(
                 data_ingestion_artifact=data_ingestion_artifact
             )
+
+            if data_validation_artifact.validation_status == True:
+                model_trainer_artifact = self.start_model_trainer()
+            
+            else:
+                raise Exception("Your data is not in correct format")
 
         
         except Exception as e:
---
+++ README.dataset.txt
@@ -0,0 +1,6 @@
+# crowd > 2022-10-26 5:42pm
+https://universe.roboflow.com/object-detection/crowd-ichka
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
